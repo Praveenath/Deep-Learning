b6ce445205d492151173eaf9637d22ac539a89d1
slpraveenath <praveenath@datamining.com.sg>
2019-11-21 23:04:00 +0530

    Added essential pipelines

---
+++ Pytorch/Basics/essential_pipelines.py
@@ -0,0 +1,90 @@
+# ================================================================== #
+#                         Input pipline                           #
+# ================================================================== #
+
+# Download and construct CIFAR-10 dataset.
+train_dataset = torchvision.datasets.CIFAR10(root='../../data/',
+                                             train=True, 
+                                             transform=transforms.ToTensor(),
+                                             download=True)
+
+# Fetch one data pair (read data from disk).
+image, label = train_dataset[0]
+print (image.size())
+print (label)
+
+# Data loader (this provides queues and threads in a very simple way).
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                           batch_size=64, 
+                                           shuffle=True)
+
+# When iteration starts, queue and thread start to load data from files.
+data_iter = iter(train_loader)
+
+# Mini-batch images and labels.
+images, labels = data_iter.next()
+
+# Actual usage of the data loader is as below.
+for images, labels in train_loader:
+    # Training code should be written here.
+    pass
+
+
+# ================================================================== #
+#                Input pipline for custom dataset                 #
+# ================================================================== #
+
+# You should build your custom dataset as below.
+class CustomDataset(torch.utils.data.Dataset):
+    def __init__(self):
+        # TODO
+        # 1. Initialize file paths or a list of file names. 
+        pass
+    def __getitem__(self, index):
+        # TODO
+        # 1. Read one data from file (e.g. using numpy.fromfile, PIL.Image.open).
+        # 2. Preprocess the data (e.g. torchvision.Transform).
+        # 3. Return a data pair (e.g. image and label).
+        pass
+    def __len__(self):
+        # You should change 0 to the total size of your dataset.
+        return 0 
+
+# You can then use the prebuilt data loader. 
+custom_dataset = CustomDataset()
+train_loader = torch.utils.data.DataLoader(dataset=custom_dataset,
+                                           batch_size=64, 
+                                           shuffle=True)
+
+
+# ================================================================== #
+#                        Pretrained model                         #
+# ================================================================== #
+
+# Download and load the pretrained ResNet-18.
+resnet = torchvision.models.resnet18(pretrained=True)
+
+# If you want to finetune only the top layer of the model, set as below.
+for param in resnet.parameters():
+    param.requires_grad = False
+
+# Replace the top layer for finetuning.
+resnet.fc = nn.Linear(resnet.fc.in_features, 100)  # 100 is an example.
+
+# Forward pass.
+images = torch.randn(64, 3, 224, 224)
+outputs = resnet(images)
+print (outputs.size())     # (64, 100)
+
+
+# ================================================================== #
+#                      Save and load the model                    #
+# ================================================================== #
+
+# Save and load the entire model.
+torch.save(resnet, 'model.ckpt')
+model = torch.load('model.ckpt')
+
+# Save and load only the model parameters (recommended).
+torch.save(resnet.state_dict(), 'params.ckpt')
+resnet.load_state_dict(torch.load('params.ckpt'))
